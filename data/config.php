a42a3b59f99d4ecce3abc1119e657e229c0bb7c0
root <root@iZwz9gi5fb5edy4ssapb07Z.(none)>
2017-12-17 14:56:30 +0800

    first commit

---
+++ data/config.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'DB_TYPE'   => 'mysql',
+    'DB_HOST'   => 'localhost',
+    'DB_USER'   => 'root',
+    'DB_PWD'   => '',
+    'DB_NAME'   => 'yoyo',
+    'DB_PREFIX'   => 'yoyo_',
+    'DB_PORT'   => '3306',
+    'DB_CHARSET'   => 'utf8',
+);
