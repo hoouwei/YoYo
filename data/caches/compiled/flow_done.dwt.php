00bdc2be770ce3e62f633abfdacf6a7efa5ba071
houwei <hw2900@126.com>
2017-12-27 11:37:53 +0800

    front is done

--- data/caches/compiled/flow_done.dwt.php
+++ data/caches/compiled/flow_done.dwt.php
@@ -18,16 +18,10 @@
 </head>
 <body>
 <div class="con">
-  <div class="ect-bg">
-  <header class="ect-header ect-margin-tb ect-margin-lr text-center ect-bg icon-write">
-      <a href="javascript:history.go(-1)" class="pull-left ect-icon ect-icon1 ect-icon-history"></a>
-      <span><?php echo $this->_var['title']; ?></span> </header>
-
-</div>
   <section class="ect-bg-colorf ect-padding-lr flow-done">
   		<i class="glyphicon glyphicon-ok"></i>
 
-
+      <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>
       <?php if ($this->_var['pay_desc']): ?>
       <p class="ect-margin-tb"><?php echo $this->_var['lang']['select_payment']; ?>: <b><?php echo $this->_var['order']['pay_name']; ?></b></p>
       <p class="ect-margin-tb text-center" style="background-color: rgb(255, 240, 240);padding: 10px;"><?php echo $this->_var['pay_desc']; ?></p>
@@ -48,7 +42,30 @@
 
 
 
-<a id="scrollUp" href="#top" style="position: fixed; z-index: 10;"><i class="fa fa-angle-up"></i></a>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <style>
     #scrollUp {
         border-radius:100%;
